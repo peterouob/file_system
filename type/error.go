d8bbfb27715016706375e5263c84d9a896b65c47
peterouob <hellolinpeter@gmail.com>
2026-01-02 22:53:28 +0800

    feat: centralized error type def

--- type/error.go
+++ type/error.go
@@ -6,11 +6,12 @@ var (
 	ErrOverflow     = errors.New("convert overflow")
 	ErrNoneNegative = errors.New("convert none negative")
 
-	ErrNotFound    = errors.New("error for file not found")
-	ErrMagicNumber = errors.New("error for file magic number")
-	ErrCookie      = errors.New("error for file cookie")
-	ErrDataDeleted = errors.New("error for file data is deleted")
-	ErrCrcNotValid = errors.New("error for file crc not valid")
+	ErrNotFound       = errors.New("error for file not found")
+	ErrMagicNumber    = errors.New("error for file magic number")
+	ErrCookie         = errors.New("error for file cookie")
+	ErrDataDeleted    = errors.New("error for file data is deleted")
+	ErrCrcNotValid    = errors.New("error for file crc not valid")
+	ErrBufferTooSmall = errors.New("error for file buffer too small")
 
 	ErrToLarge = errors.New("too large")
 )
